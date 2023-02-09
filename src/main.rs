bb2551ba6c5fc84c7873a01dfe1757b437bf199b
Rowan Lea <rowan.lea@codurance.com>
2023-02-09 12:17:27 +0000

    Moved number guesser out to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+mod number_guess;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=10);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Not a valid number.");
-                continue;
-            }
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    number_guess::guess_the_number();
 }
